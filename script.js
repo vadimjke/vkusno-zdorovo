16fdc53abfb727084a200b8518a5b0ed0943dccb
vadimjke <it.vadik@gmail.com>
2020-10-20 16:49:36 +0300

    ver 0.7

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
 
-$( "#wd1-tab" ).click(function() {
-    $("#sec2-h2-bot").text("Понедельник");
-  });
+window.onscroll = function() {scrollFunction()};
 
-  $( "#wd2-tab" ).click(function() {
-    $("#sec2-h2-bot").text("Вторник")
-  });
-
-  $( "#wd3-tab" ).click(function() {
-    $("#sec2-h2-bot").text("Среда")
-  });
-
-  $( "#wd4-tab" ).click(function() {
-    
-    $("#sec2-h2-bot").text("Четверг")
-  });
-
-  $( "#wd5-tab" ).click(function() {
-    $("#sec2-h2-bot").text("Пятница")
-  });
-
-  $( "#wd6-tab" ).click(function() {
-    $("#sec2-h2-bot").text("Суббота")
-  });
-
-  $( "#wd7-tab" ).click(function() {
-    $("#sec2-h2-bot").text("Воскресенье") 
-  });
+function scrollFunction() {
+  var navik = document.getElementById("navbar");
+  var shapochka = document.getElementById("shapochka");
+  var logoz = document.getElementById("logoz")
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    navik.classList.add("fixed-nav-bar");
+    logoz.style.height = "50px";
+    shapochka.classList.add("hidden");
+  } else {
+    navik.classList.remove("fixed-nav-bar");
+    navik.style.padding = "0";
+    logoz.style.height = "80px";
+    shapochka.classList.remove("hidden");
+  }
+}
 
 
-  $(".dot[data-theme]").click(function() {
-    $("head link#theme").attr("href", $(this).data("theme"));
-  });
+$('#menuExample').on('slide.bs.carousel', function (e) {
+
+
+  var mHeader = $('#menuExampleHeader') ;
+
+  if (e.to == 0) {
+    mHeader.html("Понедельник");
+  }
+  else if (e.to == 1) {
+    mHeader.html("Вторник");
+  }
+  else if (e.to == 2) {
+    mHeader.html("Среда");
+  }
+  else if (e.to == 3) {
+    mHeader.html("Четверг");
+  }
+  else if (e.to == 4) {
+    mHeader.html("Пятница");
+  }
+  else if (e.to == 5) {
+    mHeader.html("Суббота");
+  }
+  else if (e.to == 6) {
+    mHeader.html("Воскресенье");
+  }
+  
+})
+
