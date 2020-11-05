c67fa165c17a08dd118658639034d6b70adfbbf0
vadimjke <it.vadik@gmail.com>
2020-11-05 09:44:23 +0300

    ver 0.9

--- script.js
+++ script.js
@@ -3,20 +3,29 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   var navik = document.getElementById("navbar");
-  var shapochka = document.getElementById("shapochka");
+  // var shapochka = document.getElementById("shapochka");
   var logoz = document.getElementById("logoz");
   var glavnaya = document.getElementById("glavnaya");
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  var bar = document.getElementById("vadikscrollbar");
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     navik.classList.add("fixed-nav-bar");
     // logoz.style.height = "50px";
-    shapochka.classList.add("hidden");
+    // shapochka.classList.add("hidden");
+    bar.classList.add("s-block");
+    bar.classList.remove("s-none");
   } else {
     navik.classList.remove("fixed-nav-bar");
     navik.style.padding = "0";
     // logoz.style.height = "80px";
-    shapochka.classList.remove("hidden");
+    // shapochka.classList.remove("hidden");
     glavnaya.classList.add("active");
+    bar.classList.add("s-none");
+    bar.classList.remove("s-block");
   }
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
 
 
@@ -48,4 +57,5 @@ $('#menuExample').on('slide.bs.carousel', function (e) {
   }
   
 })
+
 
