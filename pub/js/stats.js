36d6695712cb69cd0d4b65ea5c0606ab630377f4
unknown <C:\Users\dark\AppData\Roaming\The Bat!>
2020-12-11 16:27:00 +0300

    ver 1.4.1

--- pub/js/stats.js
+++ pub/js/stats.js
@@ -1,55 +1,53 @@
-$(document).ready(function () {
-  var requestURL =
-    "http://127.0.0.1:3000/votes/json";
-  var request = new XMLHttpRequest();
-  request.open("GET", requestURL);
-  request.responseType = "json";
-  request.send();
+$(document).ready(function() {
+    var requestURL =
+        "https://vkusno-zdorovo.ru/votes/json";
+    var request = new XMLHttpRequest();
+    request.open("GET", requestURL);
+    request.responseType = "json";
+    request.send();
 
-  request.onload = function() {
-    var myData = request.response;
-    var saladsVotes = myData[0].votes;
-    var otherVotes = myData[1].votes;
-    var soupsVotes = myData[2].votes;
-    var meatVotes = myData[3].votes;
-    var sandwichVotes = myData[4].votes;
-    var pechkaVotes = myData[5].votes;
-
-
+    request.onload = function() {
+        var myData = request.response;
+        var saladsVotes = myData[0].votes;
+        var otherVotes = myData[1].votes;
+        var soupsVotes = myData[2].votes;
+        var meatVotes = myData[3].votes;
+        var sandwichVotes = myData[4].votes;
+        var pechkaVotes = myData[5].votes;
 
 
 
 
 
-  // Initialize Chart using jQuery selector.
-  // Get the chart container element.
-  var chartContainer = $("#chartContainer");
-  // Check if the element exists in the DOM.
-  if (chartContainer.length === 1) {
-    // Construct the options for the chart.
-    var options = {
-      animationEnabled: true,
-      theme: "light1",
-      title: {
-        text: "ПГУ буфет",
-      },
-      data: [
-        {
-          type: "column",
-          dataPoints: [
-            { label: "Супы", y: soupsVotes },
-            { label: "Салаты", y: saladsVotes },
-            { label: "Мясные блюда", y: meatVotes },
-            { label: "Сэндвичи", y: sandwichVotes },
-            { label: "Выпечка", y: pechkaVotes },
-            { label: "Свой вариант", y: otherVotes },
-          ],
-        },
-      ],
-    };
-    // Initialize the chart.
-    $("#chartContainer").CanvasJSChart(options);
-  }
+
+
+        // Initialize Chart using jQuery selector.
+        // Get the chart container element.
+        var chartContainer = $("#chartContainer");
+        // Check if the element exists in the DOM.
+        if (chartContainer.length === 1) {
+            // Construct the options for the chart.
+            var options = {
+                animationEnabled: true,
+                theme: "light1",
+                title: {
+                    text: "ПГУ буфет",
+                },
+                data: [{
+                    type: "column",
+                    dataPoints: [
+                        { label: "Супы", y: soupsVotes },
+                        { label: "Салаты", y: saladsVotes },
+                        { label: "Мясные блюда", y: meatVotes },
+                        { label: "Сэндвичи", y: sandwichVotes },
+                        { label: "Выпечка", y: pechkaVotes },
+                        { label: "Свой вариант", y: otherVotes },
+                    ],
+                }, ],
+            };
+            // Initialize the chart.
+            $("#chartContainer").CanvasJSChart(options);
+        }
 
 
 
@@ -65,7 +63,7 @@ $(document).ready(function () {
 
 
 
-  }
+    }
 
 
 
